e02a08e0d8d290c989f7ca7aa49cbb415365981b
Rama Krushna <ram.grpt@gmail.com>
2026-08-26 22:38:56 +0530

    feat: updated app/**

--- app/app.py
+++ app/app.py
@@ -3,3 +3,4 @@
 print ("Hello, people")
 print ("Iam python3 version")
 print ("Welcome from Github Actions")
+print ("check this file executing now..")
--- app/app.sh
+++ app/app.sh
@@ -4,3 +4,7 @@ echo "This is for test the github Actions trigger path based and branch based"
 echo "I am executed and printed the welcome notes below"
 
 echo "Welcom to GithubActions workflows"
+echo  "--------------------------"
+echo $USER
+echo "Hello $USER, how are you..?"
+echo "--------------------------"
