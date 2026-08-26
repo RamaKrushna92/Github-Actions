47798e75775fda985a0b1f54da2b4b4e203820f7
Rama Krushna <ram.grpt@gmail.com>
2026-08-26 21:15:15 +0530

    testing pullrequest event

---
+++ app/app.py
@@ -0,0 +1,5 @@
+# /bin/python
+
+print ("Hello, people")
+print ("Iam python3 version")
+print ("Welcome from Github Actions")
---
+++ app/app.sh
@@ -0,0 +1,6 @@
+# /bin/bash
+
+echo "This is for test the github Actions trigger path based and branch based"
+echo "I am executed and printed the welcome notes below"
+
+echo "Welcom to GithubActions workflows"
