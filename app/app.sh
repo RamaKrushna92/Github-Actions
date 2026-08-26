18b3b568f0e73be0e72ee89ae73073f93455d2d8
Rama Krushna <ram.grpt@gmail.com>
2026-08-26 22:50:46 +0530

    feat: updated both files under app

--- app/app.sh
+++ app/app.sh
@@ -3,7 +3,7 @@
 echo "This is for test the github Actions trigger path based and branch based"
 echo "I am executed and printed the welcome notes below"
 
-echo "Welcom to GithubActions workflows"
+echo "Welcom to GithubActions workflows."
 echo  "--------------------------"
 echo "Hello $USER, how are you..?"
 echo "--------------------------"
