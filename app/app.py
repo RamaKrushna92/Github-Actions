07daedf1df90dca77a52ffb2b15a6dd7b0dc8404
Rama Krushna <ram.grpt@gmail.com>
2026-08-26 23:08:17 +0530

    feat: syntax updated in app.py

--- app/app.py
+++ app/app.py
@@ -1,6 +1,5 @@
-# /bin/python
-
-print ("Hello, people")
-print ("Iam python3 version")
-print ("Welcome from Github Actions")
-print ("check this file executing now...")
+#/bin/python3
+print("Hello, people")
+print("Iam python3 version")
+print("Welcome from Github Actions")
+print("check this file executing now...")
